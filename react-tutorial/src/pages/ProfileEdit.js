2b4d5ae546e2a5a34586e800ed72bbf66c6834a1
YannPossboss <96826490+YannPossboss@users.noreply.github.com>
2023-02-07 16:49:16 +0100

    ProfileEdit Benutzerdaten kommen jetzt vom Server

--- react-tutorial/src/pages/ProfileEdit.js
+++ react-tutorial/src/pages/ProfileEdit.js
@@ -1,97 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
 
 import person from "./logo/person.png"
 import Footer from "./pageComponent/Footer"; 
 
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-class ProfileEdit extends React.Component {
-    render () {
-        return (
-            <div>
-                
-                <div class="w3-top">
-                    <div class="w3-bar w3-theme1" id="myNavbar">
-                    <br/>
-                    <br/>
-                    </div>
+import { useCookies } from "react-cookie";
+
+function ProfileEdit(props) {
+    const [cookies, setCookie, removeCookie] = useCookies(['name']);
+
+    const [role, setRole] = useState("");
+    const [username, setUsername] = useState("");
+    const [country, setCountry] = useState("");
+    const [email, setEmail] = useState("");
+    
+    axios.post("http://localhost:5000/secured/user", {token: cookies.token})
+    .then(response =>{
+        if(response.data.admin === true){
+            setRole("Admin");
+        }else{
+            setRole("User");
+        }
+        setUsername(response.data.username);
+        setCountry(response.data.country);
+        setEmail(response.data.email)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+
+    return (
+        <div>
+            
+            <div class="w3-top">
+                <div class="w3-bar w3-theme1" id="myNavbar">
+                <br/>
+                <br/>
                 </div>
-
-                <div class="propic w3-center w3-margin-top">
-                    <br/>
-                    <br/>
-                    <img src={person} alt="peron"/>
-                    <br/>
-                    <br/>
-                    <Link to="/" class="w3-button w3-light-blue">Upload profile picture</Link>
-                </div>
-
-                <div class="row">
-
-                        <div class="column1 w3-display-middle-left">
-
-                            <h1 class="w3-margin-left"> Your personal details :</h1>
-                            <br/>
-                            <h3 class="w3-margin-left">User role:</h3>
-                            <h4 class="w3-margin-left"><span3>Admin</span3></h4>
-                            <br/>
-                            <h3 class="w3-margin-left">School:</h3>
-                            <h4 class="w3-margin-left"><a href="http://hems.de/hemshome/">HEMS Darmstadt</a>, Hessen, Germany 🇩🇪</h4>
-                            <br/>
-                            <h3 class="w3-margin-left">First name:</h3>
-                            <h4 class="w3-margin-left">Paul</h4>
-                            <br/>
-                            <h3 class="w3-margin-left">Last name:</h3>
-                            <h4 class="w3-margin-left">Sas</h4>
-                            <br/>
-                            <h3 class="w3-margin-left">E-mail:</h3>
-                            <h4 class="w3-margin-left">paulgaming4k@gmail.com</h4>
-                            <br/>
-
-                        </div>
-
-                        <div class="column2 w3-center w3-display-middle-right">
-
-                            <h1>Edit personal details :</h1>
-
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-
-                            <div>
-                                <label for="uname"><b>Edit First Name</b></label>
-                                <br/>
-                                <input type="text" placeholder="Enter new firstname" name="uname" required/>
-                                <br/>
-                                <label for="uname"><b>Edit Last Name</b></label>
-                                <br/>
-                                <input type="text" placeholder="Enter new lastname" name="uname" required/>
-                                <br/>
-                                <label for="uname"><b>Edit User School</b></label>
-                                <br/>
-                                <input type="text" placeholder="Enter new school name" name="uname" required/>
-                                <br/>
-                                <label for="uname"><b>Edit Your E-mail</b></label>
-                                <br/>
-                                <input type="text" placeholder="Enter new e-mail" name="uname" required/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <Link to="frgt_pass.html" class="w3-button w3-light-blue"><i class="fa fa-gears w3-margin-right"></i>Change your Password</Link>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                            </div>
-
-                        </div>
-
-                </div>
-                <Footer></Footer>
             </div>
-        );
-    }
+
+            <div class="propic w3-center w3-margin-top">
+                <br/>
+                <br/>
+                <img src={person} alt="peron"/>
+                <br/>
+                <br/>
+                <Link to="/" class="w3-button w3-light-blue">Upload profile picture</Link>
+            </div>
+
+            <div class="row">
+
+                    <div class="column1 w3-display-middle-left">
+
+                        <h1 class="w3-margin-left"> Your personal details :</h1>
+                        <br/>
+                        <h3 class="w3-margin-left">Role:</h3>
+                        <h4 class="w3-margin-left">{role}</h4>
+                        <br/>
+                        <h3 class="w3-margin-left">Username:</h3>
+                        <h4 class="w3-margin-left">{username}</h4>
+                        <br/>
+                        <h3 class="w3-margin-left">Country:</h3>
+                        <h4 class="w3-margin-left">{country}</h4>
+                        <br/>
+                        <h3 class="w3-margin-left">E-mail:</h3>
+                        <h4 class="w3-margin-left">{email}</h4>
+                        <br/>
+
+                    </div>
+
+                    <div class="column2 w3-center w3-display-middle-right">
+
+                        <h1>Edit personal details : DONT WORK IN THE MOMENT</h1>
+
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+
+                        <div>
+                            <label for="uname"><b>Username</b></label>
+                            <br/>
+                            <input type="text" placeholder="Enter new Username" name="uname" required/>
+                            <br/>
+                            <label for="uname"><b>Country</b></label>
+                            <br/>
+                            <input type="text" placeholder="Enter new Country" name="uname" required/>
+                            <br/>
+                            <label for="uname"><b>E-mail</b></label>
+                            <br/>
+                            <input type="text" placeholder="Enter new E-mail" name="uname" required/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <Link to="frgt_pass.html" class="w3-button w3-light-blue"><i class="fa fa-gears w3-margin-right"></i>Change your Password</Link>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                        </div>
+
+                    </div>
+
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+    
 }
 export default ProfileEdit;

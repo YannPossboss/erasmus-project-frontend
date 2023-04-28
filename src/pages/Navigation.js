ef9c4b37efdb5c9b5a8845b86254448df5331d72
demaschine <96997586+demaschine@users.noreply.github.com>
2023-04-28 02:02:00 +0200

    Update Navigation.js

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -49,18 +49,18 @@ function Navigation(props){
                 </div>
 
                 <div class="w3-center margintop">
-                    <h1>Erasmus Recipes.</h1>
-                    <h2>On this website you can exchange your Recipies with others arround the World. </h2>
+                    <h1>Erasmus recipes.</h1>
+                    <h2>On this website you can exchange your recipes with others around the world. </h2>
                 </div>
 
                 <div class="landingnew w3-hide-small">
-                    <Link to="/login" onClick={LoginTest}><input type="submit" value="Create new Recipe +"/></Link>
-                    <Link to="/recipe"><input type="submit" value="Show me some Recipes !"/></Link>
+                    <Link to="/login" onClick={LoginTest}><input type="submit" value="Create new recipe +"/></Link>
+                    <Link to="/recipe"><input type="submit" value="Show me some recipes !"/></Link>
                 </div>
 
                 <div class="w3-hide-middle w3-hide-large">
-                    <Link to="/login" onClick={LoginTest}><input type="submitNavi" value="Create new Recipe +"/></Link>
-                    <Link to="/recipe"><input type="submitNavi" value="Show me some Recipes !"/></Link>
+                    <Link to="/login" onClick={LoginTest}><input type="submitNavi" value="Create new recipe +"/></Link>
+                    <Link to="/recipe"><input type="submitNavi" value="Show me some recipes !"/></Link>
                 </div>
                 <Footer></Footer>
             </div>
